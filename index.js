b59254094627598af2e422157c0bf2fff5db8cc2
Ashley Owens <ash4741@gmail.com>
2019-03-30 14:32:21 -0400

    First Session Adaptation Push

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const express = require('express');
 const helmet = require('helmet');
 const bcrypt = require('bcrypt');
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex');
 
 const db = require('./dbConfig.js');
 const Users = require('./users/users-module.js');
 
 const server = express();
+const sessionOptions = {
+  name: 'Lambdaschool',
+  secret: 'Enjoing lifes little secrets',
+  cookie: {
+    maxAge: 1000 * 60 * 60, //1000 = 1 second * 60 = 1 minute * 60 = 1 hour * 2 = 2 hour
+    secure: false, //false by default
+  },
+  httpOnly: true,
+  resave: false,
+  saveUninitialized: false,
+  
+  store: new KnexSessionStore({
+    knex: require('./dbConfig.js'),
+    tablename: 'sessions',
+    sidfieldname: 'sid',
+    createtable: true,
+    clearInterval: 1000 * 60 * 60
+  }),
+}
 
+server.use(session(sessionOptions));
 server.use(helmet());
 server.use(express.json());
 
@@ -40,6 +62,7 @@ server.post('/api/login', (req, res) => {
     .then(user => {
     // check that pwds match
       if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.user = user;
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: 'You shall not pass!' });
@@ -81,6 +104,23 @@ server.post('/api/users', restricted, (req, res) => {
     })
     .catch(err => res.send(err));
 });
+
+// Logout to end session
+router.get('/logout', (req, res) => {
+  if(req.session) {
+    req.session.destroy( err => {
+      if(err) {
+        res.send('A problem occured while trying to logout');
+      }
+      else{
+        res.send('You have successfully logged out!');
+      }
+    });
+  }
+  else {
+    res.end();
+  }
+})
 
 const port = process.env.PORT || 9090;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
